e518b123f45a09240acf3049084fd17f9d96f739
John Whitlock <jwhitlock@mozilla.com>
2018-02-03 08:42:16 -0600

    New Jekyll images are faster on macOS
    With 3.6.2 and beyond, the Jekyll docker image uses Bundle rather than
    Depends, which allows limiting chown to select folders. This speeds up
    startup on macOS to 5-10 seconds for chown and 1 minute to serving
    content.

--- docker-run.sh
+++ docker-run.sh
@@ -4,5 +4,4 @@ export JEKYLL_VERSION=3.5
 export JEKYLL_DEBUG=1
 export JEKYLL_UID=$(id -u)
 CMD="jekyll serve --watch --drafts"
-echo "It will take 2-3 minutes, with no output, to start on macOS."
 docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll:$JEKYLL_VERSION $CMD
